fa7afea88545d7c7e4de8725e44909d765eb0d8f
LeoNavarro1197 <ricardomontolivo1197@gmail.com>
2022-10-10 19:50:44 -0500

    tercer commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 
 let slogan = document.getElementById('slogan')
 let btn = document.getElementById('btn')
+let fondo = document.getElementById('fondo')
+let fondoParticulas = document.getElementById('fondoParticulas')
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     
     btn.style.marginTop = value * 0.7 + 'px';
-    slogan.style.marginBottom =   value* -0.85  + 'px';
+    fondo.style.marginLeft = value * 0.7 + 'px';
+    fondoParticulas.style.marginLeft = value * 0.2 + 'px';
+    //btn.style.opacity = value + 0.1;
+    slogan.style.marginBottom = value* -0.85  + 'px';
     
 })
